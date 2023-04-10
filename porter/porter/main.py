60ab0215d8c5eea2bd6a3c5a7dcfc1f06ff8eba5
Oskar Korczak <oskar.korczak@protonmail.com>
2023-04-11 00:34:27 +0200

    almost there

--- porter/porter/main.py
+++ porter/porter/main.py
@@ -5,14 +5,11 @@ from langchain.memory import ConversationBufferMemory
 from langchain.utilities import BashProcess
 
 prefix = """
-Your task is to port js codebase to ts.
-You have to work in steps.
-1. count all remaining js files. If count = 0 finish. If count is > 0 go to step 2
-2. pick random js file from directory
-3. change its extension to tsx
-4. validate file
-5. if file is valid go to step 1
-6. if file is invalid go to step 2
+your task is to fix the typescript file.
+you work in steps.
+1. validate the file
+2. fix the file
+3. go to step 1
 You have access to the following tools:"""
 suffix = """Begin!"
 
@@ -21,7 +18,7 @@ Question: {input}
 {agent_scratchpad}"""
 
 llm = ChatOpenAI(temperature=0.0)
-bash = BashProcess()
+bash = BashProcess(return_err_output=True)
 tools = [
     Tool(
         name="bash",
@@ -29,24 +26,9 @@ tools = [
         description="usefull for running bash commands",
     ),
     Tool(
-        name="count remaining js files",
-        func=lambda x: bash.run("find . -name '*.js' | wc -l"),
-        description="usefull for counting remaining js files",
-    ),
-    Tool(
-        name="pick random js file",
-        func=lambda x: bash.run("find . -name '*.js' | shuf -n 1"),
-        description="usefull for picking random js file",
-    ),
-    Tool(
-        name="validate file",
-        func=lambda x: bash.run("npx tsc {x}"),
-        description="usefull for validating file",
-    ),
-    Tool(
-        name="show whats wrong with file",
-        func=lambda x: bash.run("npx tsc {x} 2>&1 || exit 0"),
-        description="usefull for showing whats wrong with file",
+        name="validate",
+        func=lambda x: bash.run("npx tsc"),
+        description="usefull for validating typescript files",
     ),
 ]
 
@@ -66,4 +48,4 @@ agent_chain = AgentExecutor.from_agent_and_tools(
 )
 
 
-agent_chain.run(input="start")
+agent_chain.run(input="your file to fix is ./pages/index.tsx")
